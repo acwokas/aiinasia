5b457ae07f26d3b5425d2027e40ef85e4d697f9f
acwokas <me@adrianwatkins.com>
2026-02-23 00:03:26 +0800

    Create index.ts

---
+++ src/components/tiptap/index.ts
@@ -0,0 +1,6 @@
+export { default as TipTapEditor } from './TipTapEditor';
+export { default as TipTapToolbar } from './TipTapToolbar';
+export { PromptBox } from './extensions/PromptBox';
+export { YouTubeEmbed } from './extensions/YouTubeEmbed';
+export { SocialEmbed } from './extensions/SocialEmbed';
+export { ImageFigure } from './extensions/ImageFigure';
